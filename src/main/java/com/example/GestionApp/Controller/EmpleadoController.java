d12a2c402470785f3d42df4eeb5737ffccb5972b
Brian Muñoz <brozs16@gmail.com>
2025-04-25 08:25:53 -0300

    Se implementaron las vistas de thymeleaf

--- src/main/java/com/example/GestionApp/Controller/EmpleadoController.java
+++ src/main/java/com/example/GestionApp/Controller/EmpleadoController.java
@@ -5,35 +5,80 @@ import com.example.GestionApp.Service.EmpleadoServicio;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.ui.Model;
 
 import java.util.List;
 import java.util.Optional;
 
 @Controller
-@RequestMapping("/empleados")
+@RequestMapping("/")
 public class EmpleadoController {
     @Autowired
     private EmpleadoServicio empleadoServicio;
 
-    @GetMapping("/empleados")
-    private List<Empleado> listarEmpleados(){
-        return empleadoServicio.traerEmpleados();
+    @GetMapping("/")
+    public String inicio() {
+        return "index"; // Debe coincidir con index.html
     }
+
+    @GetMapping("/empleados")
+    private String listarEmpleados(Model model){
+        model.addAttribute("empleados",empleadoServicio.traerEmpleados());
+        return "empleados";
+    }
+
     @GetMapping("/empleados/{id}")
     private Optional<Empleado> traerEmpleado(@PathVariable long id){
         return empleadoServicio.buscarEmpleado(id);
     }
-    @DeleteMapping("/empleados/{id}/delete")
-    private void borrarEmpleado(@PathVariable long id){
-        empleadoServicio.borrarEmpleado(id);
+
+    @GetMapping("/empleados/nuevo")
+    private String formularioAgregadoEmpleado(Model model){
+        model.addAttribute("empleado",new Empleado());
+        return "crear-empleado";
     }
+
+    @PostMapping("/empleados/agregar")
+    private String agregarEmpleado(@ModelAttribute("empleado") Empleado e){
+        empleadoServicio.registrarEmpleado(e);
+        return "redirect:/empleados";
+    }
+    @GetMapping("/empleados/{id}/editar")
+    private String formularioEditadoEmpleado(Model model,@PathVariable long id){
+        Empleado e=empleadoServicio.buscarEmpleado(id).get();
+        model.addAttribute("empleado",e);
+        return "editar-empleado";
+    }
+
+    @DeleteMapping("/empleados/{id}/eliminar")
+    private String eliminarEmpleado(@PathVariable long id){
+        empleadoServicio.borrarEmpleado(id);
+        return "redirect:/empleados";
+    }
+    @PostMapping("/empleados/{id}/editar")
+    private String editarEmpleado(@PathVariable long id,@ModelAttribute("empleado") Empleado e){
+        empleadoServicio.actualizarEmpleado(e,id);
+        return "redirect:/empleados";
+    }
+    @ExceptionHandler(Exception.class)
+    public String manejarErrores(Model model,Exception ex){
+        model.addAttribute("error",ex.getMessage());
+        return "error";
+    }
+
+    /*@PutMapping("/empleados/{id}/editar")
+    private Empleado editarEmpleado(@RequestBody Empleado e,@PathVariable long id){
+        return empleadoServicio.actualizarEmpleado(e,id);
+    }
+
+
     @PostMapping("/empleados/agregar")
     private Empleado agregarEmpleado(@RequestBody Empleado e){
         return empleadoServicio.registrarEmpleado(e);
     }
-    @PutMapping("/empleados/{id}/editar")
-    private Empleado editarEmpleado(@RequestBody Empleado e,@PathVariable long id){
-        return empleadoServicio.actualizarEmpleado(e,id);
-    }
+    @DeleteMapping("/empleados/{id}/delete")
+    private void borrarEmpleado(@PathVariable long id){
+        empleadoServicio.borrarEmpleado(id);
+    }*/
 
 }
--- src/main/java/com/example/GestionApp/Repo/EmpleadoRepo.java
+++ src/main/java/com/example/GestionApp/Repo/EmpleadoRepo.java
@@ -1,6 +1,7 @@
 package com.example.GestionApp.Repo;
 
 import com.example.GestionApp.Models.Empleado;
+import org.hibernate.query.Page;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface EmpleadoRepo extends JpaRepository<Empleado,Long> {
